29ecaed73480c4572db9b9444f074f833f53cca5
murphyqwek <113290803+murphyqwek@users.noreply.github.com>
2022-10-07 23:42:14 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-CLIENT_SECRET_FILE = 'client_secret.json'
+CLIENT_SECRET_FILE = 'client_secrets.json'
 API_SERVICE_NAME = 'drive'
 API_VERSION = 'v3'
 SCOPES = ["https://www.googleapis.com/auth/drive"]
