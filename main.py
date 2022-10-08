155ee07c4ee677c65d3752e89c075e0776a139f0
nurphyqwek <qwekinaft@list.ru>
2022-10-08 22:59:42 +0700

    Updated

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #import Telegram_bot as telegram_bot
-from Vk_bot import Vk_bot
+from Vk_bot.Vk_bot import Vk_bot
+from Conspects_storage import Conspects_storage
 
 def main():
-    vk_bot = Vk_bot()
+    conspects = Conspects_storage()
+    vk_bot = Vk_bot(conspects)
     vk_bot.start()
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
+#Google Drive API
 CLIENT_SECRET_FILE = 'tokens\client_secrets.json'
 API_SERVICE_NAME = 'drive'
 API_VERSION = 'v3'
 SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+#Vk API
 VK_API_KEY = "vk1.a.CUz_LSMVkFb7MSgAfYfh1skRRFZqOadLj6YW6hIuYYFBs3hKKeM7dKorZdNLpizrZ2YL6OAQsOU6BsD3ZDVxPV8Jy1rxoImqit7ERQSev-x1Tdm9AOa539MnGXq-dqmjdWFsMsIJOWKrRTo89ZPdU8XJ0EMb4szu0UhcWfuUGej7mkea4FDIcchtkr7I7Nvl"
 ADMIN_ID_VK = ""
+
+#Telegram API
+
+
+#
